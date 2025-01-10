66073913fd6004e0e1e6edcefb58373d42eb2145
JaegerwaldDev <86529399+JaegerwaldDev@users.noreply.github.com>
2025-01-10 14:40:26 +0100

    update repo

--- app/src/main/java/dev/nexulien/nexoid/Constants.java
+++ app/src/main/java/dev/nexulien/nexoid/Constants.java
@@ -1,5 +1,5 @@
 package dev.nexulien.nexoid;
 
 public class Constants {
-    public static final String JS_BUNDLE_URL = "https://github.com/Nexulien/Nexulien/releases/download/devbuild/browser.js";
+    public static final String JS_BUNDLE_URL = "https://github.com/Nexulien/NexulienMobile/releases/download/devbuild/browser.js";
 }
